27a1f4c6d0e80844d0a9c3a3c52b97a7e7c21424
Cris <crisherrera024@gmail.com>
2023-12-11 13:39:11 -0600

    Created SpringBoot Project

---
+++ Neon-Backend/src/main/java/com/cognixia/jump/NeonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeonBackendApplication.class, args);
+    }
+
+}
---
+++ Neon-Backend/src/test/java/com/cognixia/jump/NeonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
