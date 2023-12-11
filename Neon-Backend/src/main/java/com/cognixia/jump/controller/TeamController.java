34e1527b842fe151b2a1d26dd1a83b5e328096e4
Brian Hitchin <brianleehitchin@gmail.com>
2023-12-11 14:26:41 -0800

    add team some

--- Neon-Backend/src/main/java/com/cognixia/jump/controller/TeamController.java
+++ Neon-Backend/src/main/java/com/cognixia/jump/controller/TeamController.java
@@ -5,23 +5,44 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.model.Team;
 import com.cognixia.jump.repository.TeamRepository;
+import com.cognixia.jump.service.TeamService;
 
 @RestController
 @RequestMapping("/api")
 public class TeamController {
 
 	@Autowired
-	TeamRepository teamRepo;
+	TeamService service;
 	
 	@GetMapping("/teams")
 	public List<Team> getAllTeams() {
-		return teamRepo.findAll();		
+		return service.getAllTeams();		
+	}
+	
+	@GetMapping("/team")
+	public ResponseEntity<?> getTeam(@RequestHeader(value="authorization") String header ) {
+		
+		Team myTeam = service.getMyTeam(header);
+		return ResponseEntity.status(200).body(myTeam);
+		
+	}
+	
+	@CrossOrigin
+	@PostMapping("/team/add")
+	public ResponseEntity<?> addTeam(@RequestHeader(value="authorization") String header, @RequestBody Team newTeam) {
+		
+		Team createdTeam = service.
+		
 	}
 	
 }
