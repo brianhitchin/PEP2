07046a4dc89fd4fb115571a8e34626a7e8695994
Brian Hitchin <brianleehitchin@gmail.com>
2023-12-12 09:06:22 -0800

    first draft endpoint

--- Neon-Backend/src/main/java/com/cognixia/jump/service/MemberService.java
+++ Neon-Backend/src/main/java/com/cognixia/jump/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.cognixia.jump.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Service;
 import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.Manager;
 import com.cognixia.jump.model.Member;
+import com.cognixia.jump.model.Team;
 import com.cognixia.jump.repository.ManagerRepository;
 import com.cognixia.jump.repository.MemberRepository;
+import com.cognixia.jump.repository.TeamRepository;
 import com.cognixia.jump.util.JwtUtil;
 
 @Service
@@ -20,6 +23,9 @@ public class MemberService {
 	
 	@Autowired
 	ManagerRepository managerRepo;
+	
+	@Autowired
+	TeamRepository teamRepo;
 	
 	@Autowired
     JwtUtil jwtUtil;
@@ -41,4 +47,38 @@ public class MemberService {
 		return repo.memberByTeam(foundManager.getTeam().getTeam_Id());
 	}
 	
+	public boolean deleteMember(String header, Integer memberId) throws ResourceNotFoundException {
+
+		if( header == null || !header.startsWith("Bearer "))
+            throw new ResourceNotFoundException("token"); 
+
+        String jwt = header.substring(7);
+        String username = jwtUtil.extractUsername(jwt);
+		
+		List<Member> foundManagerTeamMemberList = managerRepo.findByUsername(username).get().getTeam().getMember();
+		
+		List<Integer> memberIds = null;
+		
+		for (Member m: foundManagerTeamMemberList) {
+			memberIds.add(m.getId());
+		}
+		
+		if (memberIds.contains(memberId)) {
+			repo.deleteById(memberId);
+			return true;
+		}
+		
+		return false;
+
+	}
+	
+	public Member editMember(String header, Member editedMember) throws ResourceNotFoundException {
+		
+		if( header == null || !header.startsWith("Bearer "))
+            throw new ResourceNotFoundException("token"); 
+
+        repo.save(editedMember);
+		return editedMember;
+        
+	}
 }
