a4614bf34ee5da48de7e26a25c374c281317daea
Brian Hitchin <brianleehitchin@gmail.com>
2023-12-14 13:27:26 -0800

    final

---
+++ Neon-Backend/src/main/java/com/cognixia/jump/config/CORSconfig.java
@@ -0,0 +1,19 @@
+package com.cognixia.jump.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSconfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/**");
+		
+	}
+	
+}
